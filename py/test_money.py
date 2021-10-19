2fd36739561f82a9fd75b5b2e8949ae9d634ae2a
Ray Mulligan <rmullig2@optonline.net>
2021-10-19 10:39:49 -0400

    Refactor to remove Dollar class

--- py/test_money.py
+++ py/test_money.py
@@ -1,12 +1,5 @@
 import unittest
 
-class Dollar:
-  def __init__(self, amount):
-    self.amount = amount
-
-  def times(self, multiplier):
-    return Dollar(self.amount * multiplier)
-
 class Money:
   def __init__(self, amount, currency):
     self.amount = amount
@@ -16,10 +9,11 @@ class Money:
     return Money(self.amount * multiplier, self.currency)
 
 class TestMoney(unittest.TestCase):
-  def testMultiplication(self):
-    fiver = Dollar(5)
+  def testMultiplicationInDollars(self):
+    fiver = Money(5, 'USD')
     tenner = fiver.times(2)
     self.assertEqual(10, tenner.amount)
+    self.assertEqual('USD', tenner.currency)
 
   def testMultiplicationInEuros(self):
     tenEuros = Money(10, 'EUR')
