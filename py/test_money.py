4609b2e887a8946af21ee15dbefa4e50d5c06a20
Ray Mulligan <rmullig2@optonline.net>
2021-10-19 10:59:47 -0400

    Add equality method and refactor tests to use it.

--- py/test_money.py
+++ py/test_money.py
@@ -11,25 +11,24 @@ class Money:
   def divide(self, divisor):
     return Money(self.amount / divisor, self.currency)
 
+  def __eq__(self, other):
+    return self.amount == other.amount and self.currency == other.currency
+
 class TestMoney(unittest.TestCase):
   def testMultiplicationInDollars(self):
-    fiver = Money(5, 'USD')
-    tenner = fiver.times(2)
-    self.assertEqual(10, tenner.amount)
-    self.assertEqual('USD', tenner.currency)
+    fiveDollars = Money(5, 'USD')
+    tenDollars = Money(10, 'USD')
+    self.assertEqual(tenDollars fiveDollars.times(2))
 
   def testMultiplicationInEuros(self):
     tenEuros = Money(10, 'EUR')
-    twentyEuros = tenEuros.times(2)
-    self.assertEqual(20, twentyEuros.amount)
-    self.assertEqual("EUR", twentyEuros.currency)
+    twentyEuros = Money(20, 'EUR')
+    self.assertEqual(twentyEuros, tenEuros.times(2))
 
   def testDivision(self):
     originalMoney = Money(4002, 'KRW')
-    actualMoneyAfterDivision = originalMoney.divide(4)
-    expectedMoneyAfterDivision = Money(1000.5, 'KRW')
-    self.assertEqual(expectedMoneyAfterDivision.amount, actualMoneyAfterDivision.amount)
-    self.assertEqual(expectedMoneyAfterDivision.currency, actualMoneyAfterDivision.currency)
+    moneyAfterDivision = Money(1000.5, 'KRW')
+    self.assertEqual(moneyAfterDivision, originalMoney.divide(4))
 
 if __name__ == '__main__':
   unittest.main()
